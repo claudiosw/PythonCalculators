8d7585bd1d4597ed124faae911768998e63f72ea
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2022-06-01 21:30:27 -0300

    test: Improved tests of the first calculator controller

--- tests/controllers/test_run_first_calculator_controller.py
+++ tests/controllers/test_run_first_calculator_controller.py
@@ -1,12 +1,22 @@
 from src.controllers.run_first_calculator_controller import RunFirstCalculatorController
 import pytest
 
-@pytest.mark.parametrize("input_number, expected_result", [
-    ('3', 1.630860801189084)
+
+@pytest.mark.parametrize("input_number, expected_result, success", [
+    ('3', 1.630860801189084, True),
+    ('a', None, False),
+    ('', None, False)
 ])
-def test_run_first_calculator_controller(input_number, expected_result):
+def test_run_first_calculator_controller(input_number, expected_result, success):
     calculator_informations = {
         "real_number": input_number
     }
     run_first_calculator_controller = RunFirstCalculatorController()
-    assert run_first_calculator_controller.run(calculator_informations)['result'] == expected_result
+    result = run_first_calculator_controller.run(calculator_informations)
+
+    assert result['success'] == success
+    if success:
+        assert isinstance(result['result'], float)
+        assert result['result'] == expected_result
+    else:
+        assert result['error'] == "Input is not a real number"
