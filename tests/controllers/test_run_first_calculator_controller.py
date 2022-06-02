5db3ce2f26c7eca9568c31e2468f69e8ebe5fb5a
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2022-06-02 09:33:02 -0300

    test: Separated error tests

--- tests/controllers/test_run_first_calculator_controller.py
+++ tests/controllers/test_run_first_calculator_controller.py
@@ -2,21 +2,29 @@ from src.controllers.run_first_calculator_controller import RunFirstCalculatorCo
 import pytest
 
 
-@pytest.mark.parametrize("input_number, expected_result, success", [
-    ('3', 1.630860801189084, True),
-    ('a', None, False),
-    ('', None, False)
+@pytest.mark.parametrize("input_number, expected_result", [
+    ('3', 1.630860801189084)
 ])
-def test_run_first_calculator_controller(input_number, expected_result, success):
+def test_run_first_calculator_controller(input_number, expected_result):
     calculator_informations = {
         "real_number": input_number
     }
     run_first_calculator_controller = RunFirstCalculatorController()
     result = run_first_calculator_controller.run(calculator_informations)
 
-    assert result['success'] == success
-    if success:
-        assert isinstance(result['result'], float)
-        assert result['result'] == expected_result
-    else:
-        assert result['error'] == "Input is not a real number"
+    assert isinstance(result['result'], float)
+    assert result['result'] == expected_result
+
+
+@pytest.mark.parametrize("input_number, expected_result", [
+    ('a', None),
+    ('', None)
+])
+def test_run_first_calculator_controller_error(input_number, expected_result):
+    calculator_informations = {
+        "real_number": input_number
+    }
+    run_first_calculator_controller = RunFirstCalculatorController()
+    result = run_first_calculator_controller.run(calculator_informations)
+
+    assert result['error'] == "Input is not a real number"
--- tests/controllers/test_run_third_calculator_controller.py
+++ tests/controllers/test_run_third_calculator_controller.py
@@ -2,22 +2,30 @@ from src.controllers.run_third_calculator_controller import RunThirdCalculatorCo
 import pytest
 
 
-@pytest.mark.parametrize("list_input_numbers, expected_result, success", [
-    (["1", "3", "6"], True, True),
-    (["12", "13", "14"], False, True),
-    (["12", "13", "a"], False, False),
-    (["12", "13", ""], False, False)
+@pytest.mark.parametrize("list_input_numbers, expected_result", [
+    (["1", "3", "6"], True),
+    (["12", "13", "14"], False)
 ])
-def test_run_third_calculator_controller(list_input_numbers, expected_result, success):
+def test_run_third_calculator_controller(list_input_numbers, expected_result):
     calculator_informations = {
         "list_real_numbers": list_input_numbers
     }
     run_third_calculator_controller = RunThirdCalculatorController()
     result = run_third_calculator_controller.run(calculator_informations)
 
-    assert result['success'] == success
-    if success:
-        assert isinstance(result['result'], bool)
-        assert result['result'] == expected_result
-    else:
-        assert result['error'] == "At least one input is not a real number"
+    assert isinstance(result['result'], bool)
+    assert result['result'] == expected_result
+
+
+@pytest.mark.parametrize("list_input_numbers, expected_result", [
+    (["12", "13", "a"], False),
+    (["12", "13", ""], False)
+])
+def test_run_third_calculator_controller_error(list_input_numbers, expected_result):
+    calculator_informations = {
+        "list_real_numbers": list_input_numbers
+    }
+    run_third_calculator_controller = RunThirdCalculatorController()
+    result = run_third_calculator_controller.run(calculator_informations)
+
+    assert result['error'] == "At least one input is not a real number"
