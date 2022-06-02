dfdce64981030d356ee4483311dba0a04a9fd03c
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2022-06-01 22:41:10 -0300

    test: Improved tests of the second and third calculator controllers

--- tests/controllers/test_run_second_calculator_controller.py
+++ tests/controllers/test_run_second_calculator_controller.py
@@ -1,12 +1,22 @@
 from src.controllers.run_second_calculator_controller import RunSecondCalculatorController
 import pytest
 
-@pytest.mark.parametrize("list_input_numbers, expected_result", [
-    (["1", "3", "6"], 0.05566481034726358)
+
+@pytest.mark.parametrize("list_input_numbers, expected_result, success", [
+    (["1", "3", "6"], 0.05566481034726358, True),
+    (["1", "3", "a"], None, False),
+    (["1", "3", ""], None, False)
 ])
-def test_run_second_calculator_controller(list_input_numbers, expected_result):
+def test_run_second_calculator_controller(list_input_numbers, expected_result, success):
     calculator_informations = {
         "list_real_numbers": list_input_numbers
     }
     run_second_calculator_controller = RunSecondCalculatorController()
-    assert run_second_calculator_controller.run(calculator_informations)['result'] == expected_result
+    result = run_second_calculator_controller.run(calculator_informations)
+
+    assert result['success'] == success
+    if success:
+        assert isinstance(result['result'], float)
+        assert result['result'] == expected_result
+    else:
+        assert result['error'] == "At least one input is not a real number"
--- tests/controllers/test_run_third_calculator_controller.py
+++ tests/controllers/test_run_third_calculator_controller.py
@@ -2,13 +2,22 @@ from src.controllers.run_third_calculator_controller import RunThirdCalculatorCo
 import pytest
 
 
-@pytest.mark.parametrize("list_input_numbers, expected_result", [
-    (["1", "3", "6"], True),
-    (["12", "13", "14"], False)
+@pytest.mark.parametrize("list_input_numbers, expected_result, success", [
+    (["1", "3", "6"], True, True),
+    (["12", "13", "14"], False, True),
+    (["12", "13", "a"], False, False),
+    (["12", "13", ""], False, False)
 ])
-def test_run_third_calculator_controller(list_input_numbers, expected_result):
+def test_run_third_calculator_controller(list_input_numbers, expected_result, success):
     calculator_informations = {
         "list_real_numbers": list_input_numbers
     }
     run_third_calculator_controller = RunThirdCalculatorController()
-    assert run_third_calculator_controller.run(calculator_informations)['result'] == expected_result
+    result = run_third_calculator_controller.run(calculator_informations)
+
+    assert result['success'] == success
+    if success:
+        assert isinstance(result['result'], bool)
+        assert result['result'] == expected_result
+    else:
+        assert result['error'] == "At least one input is not a real number"
