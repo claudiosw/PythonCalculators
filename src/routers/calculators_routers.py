eaaffa374b399ecfcc00eaf1386efc3edf5d039e
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2022-06-06 23:58:27 -0300

    improve: Added adapter of calculator 2

--- src/routers/calculators_routers.py
+++ src/routers/calculators_routers.py
@@ -1,4 +1,5 @@
 from src.controllers.run_first_calculator_controller import RunFirstCalculatorController
+from src.controllers.run_second_calculator_controller import RunSecondCalculatorController
 from src.helpers.request_adapter import request_adapter
 from flask import Blueprint, request, jsonify
 
@@ -14,4 +15,10 @@ calculators_bp = Blueprint(
 @calculators_bp.route('/calculators/1', methods=['POST'])
 def calculators():
     http_response = request_adapter(request, RunFirstCalculatorController())
+    return jsonify(http_response.body), http_response.status_code
+
+
+@calculators_bp.route('/calculator_2', methods=['POST'])
+def calculator_2():
+    http_response = request_adapter(request, RunSecondCalculatorController())
     return jsonify(http_response.body), http_response.status_code
--- tests/controllers/test_run_second_calculator_controller.py
+++ tests/controllers/test_run_second_calculator_controller.py
@@ -1,30 +1,34 @@
 from src.controllers.run_second_calculator_controller import RunSecondCalculatorController
+from src.models.http_types.http_request import HttpRequest
 import pytest
+import json
 
 
-@pytest.mark.parametrize("list_input_numbers, expected_result", [
-    (["1", "3", "6"], 0.05566481034726358)
+@pytest.mark.parametrize("input, expected_result", [
+    ('{"list_real_numbers": [1, 3, 6]}', 0.05566481034726358)
 ])
-def test_run_second_calculator_controller(list_input_numbers, expected_result):
-    calculator_informations = {
-        "list_real_numbers": list_input_numbers
-    }
+def test_run_second_calculator_controller(input, expected_result):
+    http_request = HttpRequest(
+        body=json.loads(input)
+    )
     run_second_calculator_controller = RunSecondCalculatorController()
-    result = run_second_calculator_controller.run(calculator_informations)
+    result = run_second_calculator_controller.run(http_request)
 
+    assert result["success"] is True
     assert isinstance(result['result'], float)
     assert result['result'] == expected_result
 
 
-@pytest.mark.parametrize("list_input_numbers, expected_result", [
-    (["1", "3", "a"], None),
-    (["1", "3", ""], None)
+@pytest.mark.parametrize("input, expected_result", [
+    ('{"list_real_numbers": [1, 3, "a"]}', None),
+    ('{"list_real_numbers": [1, 3, ""]}', None)
 ])
-def test_run_second_calculator_controller_error(list_input_numbers, expected_result):
-    calculator_informations = {
-        "list_real_numbers": list_input_numbers
-    }
+def test_run_second_calculator_controller_error(input, expected_result):
+
+    http_request = HttpRequest(
+        body=json.loads(input)
+    )
     run_second_calculator_controller = RunSecondCalculatorController()
-    result = run_second_calculator_controller.run(calculator_informations)
+    result = run_second_calculator_controller.run(http_request)
 
     assert result['error'] == "At least one input is not a real number"
